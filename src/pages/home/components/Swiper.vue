e2f579ecddd574248c6331f3bf940ccf01c13aeb
2016soft <2016soft@iMac16.local>
2018-10-19 11:16:40 +0800

    增加轮播

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,14 +1,53 @@
 <template>
-  <div>
-    swiper
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper_img" :src="item.imgUrl" alt=""/>
+        </swiper-slide>
+      <!-- <swiper-slide>
+        <img class="swiper_img" :src="item.imgUrl" alt=""/>
+        </swiper-slide> -->
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div> -->
+      <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+    </swiper>
   </div>
 </template>
 
 <script>
 export default{
-  name: 'HomeSwiper'
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      },
+      swiperList: [{
+        id: '1001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+      }, {
+        id: '1002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+      }, {
+        id: '1003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/2d/0d7dc3400a50b502.jpg_750x200_4ca95dd4.jpg'
+      }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.swiper_img{
+  width: 100%;
+}
+.wrapper >>>.swiper-pagination-bullet-active{
+  background: #fff !important;
+}
 </style>
